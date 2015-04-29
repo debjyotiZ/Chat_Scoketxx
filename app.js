35c982f6cfa32df5913282f625c3cd4e906ee5db
unknown <I069208@INLN50861713A.apj.global.corp.sap>
2015-04-29 23:07:31 +0530

    agaiqn

--- app.js
+++ app.js
@@ -21,42 +21,29 @@ http.listen(3000, function(){
 */
 
 var express = require('express');
-var app = express.createServer();
+var app = express();
 console.log("fine1");
-/*   server = require('http').createServer(app);
-   server.listen(process.env.PORT || 5000); */
-   var sio = require('socket.io');
-
-app.listen(port, function () {
-  var addr = app.address();
-  console.log('   app listening on http://' + addr.address + ':' + addr.port);
-});
+   server = require('http').createServer(app);
+    io = require('socket.io').listen(server);
+console.log("fine1");
+server.listen(process.env.PORT || 5000);
+//app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   request.sendFile('index.html');
 });
-var io = sio.listen(app)
+console.log("fine2");
 io.configure(function () { 
 io.set('transports', ['xhr-polling']);
-io.set('polling duration', 20);
+io.set('polling duration', 10);
 });
-/*io.on('connection', function(socket){
+io.on('connection', function(socket){
   
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
-*/
-
-io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
-    socket.broadcast.emit('chat message', socket.nickname, msg);
-  });
-
-  
-});
-
 console.log("fine3");
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
