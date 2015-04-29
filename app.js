158c636fee24d9ed5123d55f0bcef00d66aab822
unknown <I069208@INLN50861713A.apj.global.corp.sap>
2015-04-29 22:55:33 +0530

    agaisn

--- app.js
+++ app.js
@@ -25,24 +25,38 @@ var app = express();
 console.log("fine1");
    server = require('http').createServer(app);
    server.listen(process.env.PORT || 5000); 
-    io = require('socket.io').listen(server);
-console.log("fine1");
+    sio = require('socket.io');
 
-//app.set('port', (process.env.PORT || 5000));
+app.listen(port, function () {
+  var addr = app.address();
+  console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   request.sendFile('index.html');
 });
-console.log("fine2");
+var io = sio.listen(app)
+io.configure(function () { 
 io.set('transports', ['xhr-polling']);
 io.set('polling duration', 10);
-io.on('connection', function(socket){
+});
+/*io.on('connection', function(socket){
   
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
+*/
+
+io.sockets.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    socket.broadcast.emit('user message', socket.nickname, msg);
+  });
+
+  
+});
+
 console.log("fine3");
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
