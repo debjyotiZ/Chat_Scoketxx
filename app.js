5fe5afcc2fe1b56a5d0a2867738b0f93af1e5dc7
unknown <I069208@INLN50861713A.apj.global.corp.sap>
2015-04-29 19:06:50 +0530

    thrid

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ http.listen(3000, function(){
 
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -29,7 +31,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   request.sendFile('index.html');
 });
-
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
